8635c18204c49eea4d6edf229f4d8adb0a16a3bd
JakeFernandes98 <jakeneilaldo@hotmail.com>
2020-06-26 15:07:11 +0100

    Added DB

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,31 @@
 from django.db import models
 
 # Create your models here.
+
+class RegPizza(models.Model):
+    name = models.CharFiels(max_length=64)
+    smallPrice = models.DecimalField(decimal_places=2)
+    largePrice = models.DecimalField(decimal_places=2)
+
+class SicPizza(models.Model):
+    name = models.CharFiels(max_length=64)
+    smallPrice = models.DecimalField(decimal_places=2)
+    largePrice = models.DecimalField(decimal_places=2)
+
+class Subs(models.Model):
+    name = models.CharFiels(max_length=64)
+    smallPrice = models.DecimalField(decimal_places=2)
+    largePrice = models.DecimalField(decimal_places=2)
+
+class Pasta(models.Model):
+    name = models.CharFiels(max_length=64)
+    price = models.DecimalField(decimal_places=2)
+
+class Salads(models.Model):
+    name = models.CharFiels(max_length=64)
+    price = models.DecimalField(decimal_places=2)
+
+class Platters(models.Model):
+    name = models.CharFiels(max_length=64)
+    smallPrice = models.DecimalField(decimal_places=2)
+    largePrice = models.DecimalField(decimal_places=2)
